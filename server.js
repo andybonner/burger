b7164cfaefbe6568e83eaa44f801920a60a68524
Andy Bonner <abonner@duke.edu>
2017-08-25 07:24:29 -0400

    capitalize PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = process.env.port || 8080;
+var PORT = process.env.PORT || 8080;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
@@ -24,6 +24,6 @@ var routes = require("./controllers/burgers_controller");
 
 app.use("/", routes);
 
-app.listen(port, function(){
-  console.log("Listening on port", port);
+app.listen(PORT, function(){
+  console.log("Listening on port", PORT);
 });
